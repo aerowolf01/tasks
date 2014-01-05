693cd3811f16132582f165dfb8cb6f3197b6bfe5
Cooper LeBrun <cooperlebrun@gmail.com>
2014-01-04 17:00:27 -0800

    changed site root to task#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Tasks::Application.routes.draw do
   get "tasks/index"
+
+  root :to => 'tasks#index'
   resources :tasks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
