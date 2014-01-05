a0319a27e541d3f8442e85af35a7fb15dbe02434
Cooper LeBrun <cooperlebrun@gmail.com>
2014-01-04 20:39:47 -0800

    filled in index and made create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,11 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+  end
+
+  def create
+    params.permit!
+    Task.create params[:task]
+    redirect_to :back
   end
 end
