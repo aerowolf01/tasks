57ee88ec60d6e06254509c4aff45c0739872f0a4
Cooper LeBrun <cooperlebrun@gmail.com>
2014-01-04 22:51:54 -0800

    silenced annoying i18n messages during testing

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Tasks
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Silence annoying locale depreciation messages >:I
+    config.i18n.enforce_available_locales = true
   end
 end
